364389f7e946377c3a2abbf6e5070d6783d5b899
Sylvie Trolle <sylvie.trolle@gustaveroussy.fr>
2026-02-20 12:41:23 +0100

    feat: externalisation des routes books

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
-const Book = require("./models/Book");
+const booksRoutes = require("./routes/books");
+
+const app = express();
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch((error) => console.log("Connexion à MongoDB échouée !", error));
 
-const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -26,39 +27,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/books", (req, res, next) => {
-  this.delete.req.body._id;
-  const book = new Book({
-    ...req.body,
-  });
-  book
-    .save()
-    .then(() => res.status(201).json({ message: "Livre enregistré !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.put("/api/books/:id", (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Livre modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.delete("/api/books/:id", (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Livre supprimé !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.get("/api/books/:id", (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then((book) => res.status(200).json(book))
-    .catch((error) => res.status(404).json({ error }));
-});
-
-app.get("/api/books", (req, res, next) => {
-  Book.find()
-    .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json({ error }));
-});
+app.use("/api/books", booksRoutes);
 
 module.exports = app;
