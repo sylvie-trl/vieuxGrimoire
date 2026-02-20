e7f6a0956e61fb7ffa38e142d5ccc3d41f9d4d08
Sylvie Trolle <sylvie.trolle@gustaveroussy.fr>
2026-02-20 12:28:21 +0100

    création routes PUT et DELETE

--- backend/app.js
+++ backend/app.js
@@ -37,6 +37,18 @@ app.post("/api/books", (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 });
 
+app.put("/api/books/:id", (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Livre modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("/api/books/:id", (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Livre supprimé !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
 app.get("/api/books/:id", (req, res, next) => {
   Book.findOne({ _id: req.params.id })
     .then((book) => res.status(200).json(book))
