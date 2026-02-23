f757fc88c10e1bc32c985458fe805ca0167d0031
Sylvie Trolle <sylvie.trolle@gustaveroussy.fr>
2026-02-23 09:18:39 +0100

    fix: correction imageUrl + import uniqueValidator refactor: port serveur 4000

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bookSchema = mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  imageURL: { type: String, required: true },
+  imageUrl: { type: String, required: true },
   year: { type: Number, required: true },
   genre: { type: String, required: true },
   ratings: [{ userId: String, grade: Number }],
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const uniqueValidator = require("mongoose-unique-validator");
+const uniqueValidator = require("mongoose-unique-validator").default;
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
