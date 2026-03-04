59aa7977b483999f2db7132bdf1f3ef74145ed22
Sylvie Trolle <sylvie.trolle@gustaveroussy.fr>
2026-03-04 09:01:17 +0100

    Sécurité : déplacer le secret JWT dans une variable d’environnement (.env)

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
@@ -36,7 +38,7 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+            token: jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
               expiresIn: "24h",
             }),
           });
